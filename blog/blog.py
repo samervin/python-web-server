9b246b763f038547256a1a6dd7404fb7a59d3d5e
Sam Ervin <samuelthomaservin@gmail.com>
2018-01-30 00:17:59 -0600

    Added simple navigational footer for blog posts

--- blog/blog.py
+++ blog/blog.py
@@ -12,22 +12,27 @@ def get_blog_home_markdown():
     with open('blog/blog_home.md') as blog_home:
         blog_opener = blog_home.read()
 
+    post_list = get_blog_posts_list()
+    post_list.reverse()
+    post_list_str = ''
+    for post in post_list:
+        post_list_str += '\n- [{}]({})'.format(post.title, post.url_slug)
+    return blog_opener + post_list_str
+
+
+def get_blog_posts_list():
     post_list = []
     for root, directories, files in os.walk('blog/posts'):
         for filename in files:
-            url_slug = filename.replace('.md', '')
             with open('blog/posts/{}'.format(filename)) as post_file:
                 post_md = post_file.read()
             post_html = site_functions.md_to_html(post_md)
             post_datetime = post_html.metadata.get('datetime')
             post_title = post_html.metadata.get('title')
+            url_slug = filename.replace('.md', '')
             post_list.append(Post(filename, url_slug, post_md, post_html, post_datetime, post_title))
-    
     post_list = sorted(post_list, key=lambda p: p.datetime)
-    post_list_str = ''
-    for post in post_list:
-        post_list_str += '\n- [{}]({})'.format(post.title, post.url_slug)
-    return blog_opener + post_list_str
+    return post_list
 
 
 def get_blog_post_html(post_name):
@@ -36,4 +41,28 @@ def get_blog_post_html(post_name):
         post_html = site_functions.md_to_html(post_md)
         post_title = post_html.metadata.get('title')
         title_html = '<title>{}</title>'.format(post_title)
-        return title_html + post_html
+        footer_md = get_blog_post_footer_markdown(post_name)
+        footer_html = site_functions.md_to_html(footer_md)
+        return title_html + post_html + footer_html
+
+
+def get_blog_post_footer_markdown(post_name):
+    with open('blog/blog_post_footer.md') as footer_file:
+        footer_md = footer_file.read()
+    post_list = get_blog_posts_list()
+    
+    for index, post in enumerate(post_list):
+        if post.url_slug == post_name:
+            break
+
+    previous_post_link = '←'
+    next_post_link = '→'
+    if index > 0:
+        previous_post = post_list[index - 1]
+        previous_post_link = '[← {}]({})'.format(previous_post.title, previous_post.url_slug)
+    if index < len(post_list) - 1:
+        next_post = post_list[index + 1]
+        next_post_link = '[{} →]({})'.format(next_post.title, next_post.url_slug)
+    footer_md = footer_md.replace('{previous-post-link}', previous_post_link)
+    footer_md = footer_md.replace('{next-post-link}', next_post_link)
+    return footer_md
---
+++ blog/blog_post_footer.md
@@ -0,0 +1,5 @@
+<div class='widen' markdown='1'>
+
+{previous-post-link} {next-post-link}
+
+</div>
